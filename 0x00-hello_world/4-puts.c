fc1e05d0a2584ac53adb81d662acf1cdd8c1457a
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-08 03:43:19 +0200

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/*This function prints \"Programming is like building a multilingual puzzle */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
