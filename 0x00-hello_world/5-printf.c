a923dde645b0e0d98496282333feae4323c861dc
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-08 04:08:34 +0200

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*This programe will just print anstatement with printf*/
+
+#include <stdio.h>
+
+/**
+ * main - This is our canvas
+ * Return: 0 for a functioning program, otherwise 1
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
