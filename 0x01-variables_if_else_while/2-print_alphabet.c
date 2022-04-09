dd241bc27a57ee69bfa159879502961969e40c8c
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-09 03:05:33 +0200

    Playing the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program to print alphabets from a to z
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	return (0);
+}
