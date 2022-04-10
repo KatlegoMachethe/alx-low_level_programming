31c93a38a2a8cfb2cbc538888bdd9f014a4e3d55
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-10 03:52:28 +0200

    Unix is basically a simple operating system, but you have to be a genius to understand the symplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,8 @@
  */
 int main(void)
 {
-	char quote1[] = "and that piece of art is useful\" - D";
-	char quote2[] = "ora Korpar, 2015-10-19";
+	char quote1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote1, sizeof(quote1));
-	write(1, quote2, sizeof(quote2));
+	write(1, quote1, 59);
 	return (1);
 }
