287925638e529c77f87c651296764a516b994ea4
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-12 00:29:51 +0200

    Combination of two two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Program function
+ * Description: Program wrtes a combination of two two digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (!(a == d && b == c))
+					{
+						putchar('0' + a);
+						putchar('0' + b);
+						putchar(' ');
+						putchar('0' + c);
+						putchar('0' + d);
+
+						if (!(a + b == 18 && b + c == 17 && c == 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
