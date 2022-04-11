8dbab308b823dc2d2d17d6fcb5021e63f58eb2ce
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-12 00:04:39 +0200

    COmbination of three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,11 +16,11 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
-			for (num3 = 0; num3 <= 9; num3++)
+			for (num3 = 0; num3 < 10; num3++)
 			{
 				if (num1 < num2 && num2 < num3 && num1 != (num2 != num3))
 				{
