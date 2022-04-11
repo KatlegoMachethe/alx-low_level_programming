a4a9aa2fe30cce8c15818d16c7c037145ae36a15
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-11 23:48:31 +0200

    Combition of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,17 +16,17 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 48; num1 < 58; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = 48; num2 < 58; num2++)
+		for (num2 = 0; num2 <= 9; num2++)
 		{
-			for (num3 = 48; num3 < 58; num3++)
+			for (num3 = 0; num3 <= 9; num3++)
 			{
 				if (num1 < num2 && num2 < num3 && num1 != (num2 != num3))
 				{
-					putchar(num1);
-					putchar(num2);
-					putchar(num3);
+					putchar('0' + num1);
+					putchar('0' + num2);
+					putchar('0' + num3);
 
 					if (num1 + num2 + num3 != 7 + 8 + 9)
 					{
