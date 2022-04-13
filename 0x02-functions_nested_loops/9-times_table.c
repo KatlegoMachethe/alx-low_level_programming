f013ff3d32f2ffa6a0cb34178ab884fc851785a0
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-13 21:44:14 +0200

    Times table up to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,11 +13,11 @@ void times_table(void)
 		for (num2 = 0; num2 <= 9; num2++)
 		{
 			product = num1 * num2;
-			/*if (num1 == 0)
+			if (num2 != 0)
 			{
 				_putchar(product + '0');
 				_putchar(' ');
-			}*/
+			}
 			if (product > 9)
 			{
 				_putchar(' ');
