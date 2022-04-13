80d5c36644b192e1e7e9b9aa661f94d1deecdd09
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-13 22:20:33 +0200

    Printing every minute of the dayfrom 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute from 00:00 to 23:59
+ * Return; Nothing
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
