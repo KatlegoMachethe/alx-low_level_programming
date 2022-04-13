4ca60c7e8428ea6254f260e39cffbc39f2786bca
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-13 06:00:42 +0200

    Natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,28 +10,31 @@
  */
 void print_to_98(int n)
 {
+	int x = 0;
+	
 	if (n < 98)
 	{
-		for (n = n; n <= 98; n++)
+		for (x = n; n <= 98; n++)
 		{
-			if (n != 98)
+			if (n != x)
 			{
-				printf("%d", n);
 				_putchar(',');
 				_putchar(' ');
 			}
+			printf("%d", n);
+
 		}
 	}
 	else
 	{
-		for (n = n; n >= 98; n--)
+		for (x = n; n >= 98; n--)
 		{
-			if (n != 98)
+			if (n != x)
 			{
-				_putchar("%d", n);
 				_putchar(',');
 				_putchar(' ');
 			}
+			printf("%d", n);
 		}
 	}
 }
