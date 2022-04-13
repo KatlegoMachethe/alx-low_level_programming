6bad9631228091d9aed514c1b9f678090f219e33
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-13 02:18:30 +0200

    Printing "_putchar"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,14 @@
  */
 int main(void)
 {
-	char c[] = "_putchar";
-
-	_putchar(c);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
