c1e76d8d173f5d42cb53be04906a1aa222d1a4af
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-13 04:24:30 +0200

    Investigating signs on numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Check is number is positive or not
+ * @n: Integer argument
+ *
+ * Return: 1 for +, 0 for zero and -1 for -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}
