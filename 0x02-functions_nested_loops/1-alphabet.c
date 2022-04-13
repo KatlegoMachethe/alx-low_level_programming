4af7e46f28eebf3eddb60c6ed1d0fd7511fda594
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-13 02:58:12 +0200

    Printing lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints lowercase alphabets
+ * Return: always 0
+ */
+int main(void)
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
