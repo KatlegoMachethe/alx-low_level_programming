e6c37335644a1e00bb070e9034e821c5f04f03ee
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-13 04:13:23 +0200

    Chelcking fo letter, uppercase and lowercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 
 /**
  * _isalpha - Checks for lowercase, upercase and letters
- * @c - First argument
+ * @c: First argument
  *
  * Return: 1 for success, otherwise 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90) || (sizeof(c) > 1))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	else
 		return (0);
