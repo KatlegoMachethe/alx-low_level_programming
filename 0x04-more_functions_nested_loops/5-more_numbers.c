200515a708bd70f706c3a2cdc809f04b01e6758c
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-15 04:16:29 +0200

    Printing numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int count, number;
+
+	for (count = 0; count < 10; count++)#
+	{
+		for(number = 0; number <= 14, number++)
+		{
+			_putchar(number + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
