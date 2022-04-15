465532c39201fc2aac3201953bc882e9c38c4cf5
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-15 03:45:54 +0200

    Prints numbers from 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints number from 0 to 9
+ * excluding 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int number;
+	
+	for (number = 48; number <= 57; number++)
+	{
+		if (!(number == 50 || number == 52))
+		{
+			_putchar(number);
+		}
+	}
+	_putchar('\n');
+}
+
