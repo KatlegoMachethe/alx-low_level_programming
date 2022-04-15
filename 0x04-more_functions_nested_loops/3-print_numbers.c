da77e29c56f68f369ac9515e68857b460d505682
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-15 03:23:20 +0200

    Printing numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int counter;
+
+	for (counter = 48; counter <= 57: counter++)
+	{
+		_putchar(counter);
+	}
+	_putchar('\n');
+}
