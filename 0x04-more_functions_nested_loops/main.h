a81f805f6aa049dff605eab0ae7969b0f31f2ef0
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-16 19:45:40 +0200

    Main header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,8 +15,12 @@ void print_most_numbers(void);
 
 void more_numbers(void);
 
-void print_line(int n);
+void print_line(int);
 
 void print_diagonal(int n);
+
+void print_square(int);
+
+void print_triangle(int);
 
 #endif
