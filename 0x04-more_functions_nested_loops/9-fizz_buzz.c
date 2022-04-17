f693eac401309204faf1b7f1ea0b487e8cffc1da
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-17 18:46:14 +0200

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,20 @@ int main(void)
 
 	for (number = 1; number <= 100; number++)
 	{
-		if (number % 3 == 0)
-		{
-			printf("%s", "Fizz");
-		}
-		else if (number % 5 == 0)
-		{
-			printf("%s", "Buzz");
-		}
-		else if (number % 3 == 0 && number % 5 == 0)
+		if (number % 3 == 0 && number % 5 == 0)
 		{
 			printf("%s", "FizzBuzz");
+		}
+		else if (number % 3 == 0 || number % 5 == 0)
+		{
+			if (number % 3 == 0)
+			{
+				printf("%s", "Fizz");
+			}
+			else if (number % 5 == 0)
+			{
+				 printf("%s", "Buzz");
+			}
 		}
 		else
 		{
