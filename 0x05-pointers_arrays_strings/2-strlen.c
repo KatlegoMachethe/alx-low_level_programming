bad30b8ff99351341a7ade061d264fd9f252e82f
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-21 01:43:05 +0200

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String argument
+ * Return: Integer
+ */
+
+int _strlen(char *s)
+{
+	char pointer;
+
+	pointer = *s;
+	return (sizeof(pointer));
+}
