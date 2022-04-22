b4bc03bd486d3a86f7fcd7204a56091dcad444d0
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-22 17:19:32 +0200

    concatinating two srings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,14 +12,16 @@ char *_strcat(char *dest, char *src)
 	int len1, len2;
 	
 	len1 = 0;
+	len2 = 0;
 	while (dest[len1] != '\0')
 	{
 		len1++;
 	}
-	for (len2 = 0; src[len2] != '\0'; len2++)
+	while (src[len2] != '\0')
 	{
 		dest[len1] = src[len2];
 		len1++;
+		len2++;
 	}
 	dest[len1] = '\0';
 	return (dest);
