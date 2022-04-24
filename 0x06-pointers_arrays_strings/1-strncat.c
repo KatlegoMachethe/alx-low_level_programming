263e36df67e699309cf5b90525ba39542d126c87
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-24 03:27:19 +0200

    Concatenating two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,19 +5,25 @@
  * @dest: First string input
  * @src: Second string input
  * @n: integer input
+ * Return: New and updated dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index;
+	int index, count;
 
-	for (index = 0; src[index] != '\0' && index < n; index++)
+	index = 0;
+	while (dest[index] != '\0')
+	{
+		index++;
+	}
+	count = 0;
+	while (src[count] != src[n])
 	{
 		dest[index] = src[index];
+		count++;
+		index++;
 	}
-	for ( ; index < n; index++)
-	{
-		dest[index] = '\0';
-	}
+	dest[index] = '\0';
 	return (dest);
 }
