1791c62721821d1f6e30f0b0ef45b7073ef963f7
KatlegoMachethe <kmi.machethe@gmail.com>
2022-04-26 04:34:46 +0200

    Dealing with strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (k = i, j = 0; needle[j] != '\0'; j++, k++)
 		{
-			if (needle[j] != haystack[k] || haystack[k] !='\0')
+			if (needle[j] != haystack[k] || haystack[k] != '\0')
 			{
 				break;
 			}
