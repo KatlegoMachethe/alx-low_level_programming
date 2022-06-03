857881d2a6eab04b2ed01ec5a96996ed7d237b4a
KatlegoMachethe <kmi.machethe@gmail.com>
2022-06-03 23:02:54 +0200

    2nd bug fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,7 @@ int _strlen(char *string)
 	{
 		len++;
 	}
+	len++;
 	return (len);
 }
 
@@ -29,12 +30,12 @@ int _strlen(char *string)
 
 int create_file(const char *filename, char *text_content)
 {
-	int file,  writing;
+	int file, writing;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_CREAT, 0600);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
 		return (-1);
 	if (text_content != NULL)
